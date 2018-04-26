2258b61c49ad139b0218a93867ed9c444ccd2fc0
Evania <evan.nhea@gmail.com>
2018-04-26 14:22:52 +0700

    revision

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,5 @@
-function largestPrimeFactor(num) {
+function largestPrimeFactor() {
+  num=eval(form.num.value)
   var primes = [];
   for (i = 1; i <= num; i++) {
     if (num % i == 0) {
@@ -9,13 +10,21 @@ function largestPrimeFactor(num) {
     }
   }
   return Math.max.apply(null, primes);
+  form.result.value= eval(largestPrimeFactor;
 }
 const isPrime = num => {
   var isPrime = true;
   for (let i = 2, s = Math.sqrt(num); i <= s; i++)
     if (num % i == 0) return false;
   return num !== 1;
-};
+}
 
-module.exports = largestPrimeFactor;
 
+
+function tambah() {
+  cek(); //panggil function cek
+  a=eval(form.angka1.value); //mengisi variabel a dengan isi dari input name angka1
+  b=eval(form.angka2.value); //mengisi variabel b dengan isi dari input name angka2
+  c=a+b //menjumlahkan kedua variabel
+  form.total.value = c; //memberikan hasil penjumlahan ke input name total
+  }
